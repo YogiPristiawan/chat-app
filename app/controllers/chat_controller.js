1d70645d74f6235044c9927cf1109d1b7d097c8a
YogiPristiawan <yogistanada252@gmail.com>
2021-08-13 14:26:23 +0700

    fix import module

--- app/controllers/chat_controller.js
+++ app/controllers/chat_controller.js
@@ -1,4 +1,4 @@
-const Users = require("./../models/users");
+const Users = require("./../models/Users");
 const { Op } = require("sequelize");
 const { timeSince } = require("./../helpers/datetime");
 const Chat = require("./../models/chat");
